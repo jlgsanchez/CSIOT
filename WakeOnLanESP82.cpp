7704e389b7159d528acd393b81368a65007eec4d
Jose Luis Garcia Sanchez <j.gsanchez@udc.es>
2024-04-28 13:32:00 +0200

    Update WakeOnLanESP82.cpp

--- WakeOnLanESP82.cpp
+++ WakeOnLanESP82.cpp
@@ -5,10 +5,9 @@
 #include <WiFiUdp.h>
 
 //WiFiClient espClient;
-WiFiClientSecure espClientSecure;
 //PubSubClient client(espClient);
+WiFiClientSecure espClientSecure;
 PubSubClient client(espClientSecure);
-//const char* mqtt_server = "mqtt.armriot.com";
 WiFiUDP Udp;
 
 void reconnect();  // Declarar funcion
@@ -22,8 +21,7 @@ void setup() {
   Serial.println("Hello, ESP8266!");
   pinMode(LED_BUILTIN, OUTPUT);     // Numero de pin, variable en este caso, y el modo, output.
 
-  //WiFi.begin("munics", "BayernMunich");
-  WiFi.begin("MIWIFI_vTZw", "Joseluis19112502");
+  WiFi.begin("", "");  // Añadir el SSID y contraseña dependiendo de la red a la que se quiera conectar
 
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
@@ -35,7 +33,7 @@ void setup() {
   Serial.print("connected, Ip address=");
   Serial.println(WiFi.localIP());
 
-
+  // Fingerprint del certificado de la CA
   const uint8_t mqttCertFingerprint[] = {0xD7,0xFC,0xD6,0x8A,0xF7,0x6A,0xAA,0x06,0x8B,0x81,0x70,0xC4,0xC9,0x23,0xD0,0x92,0xFB,0x17,0x4E,0xF6};
 
   //espClientSecure.setInsecure();
@@ -48,24 +46,16 @@ void setup() {
 
   unsigned long blinkTimeStamp = 0;  // Marcas de tiempo para comprobar si ha pasado o no el tiempo
   int estadoLed = LOW;
-  String swtch = "";
+  String swtch = "";  // Variable para comprobar si ha llegado o no la orden por MQTT
 
-  unsigned int localPort = 9;      // local port to listen for UDP packets
+  unsigned int localPort = 9;      // Puerto local de la máquina objetivo escuchando por paquete WoL
 
-  // Mac address of the device you want to wake up
-  // byte mac[] = { 0xb8, 0x27, 0xeb, 0xa0, 0xe2, 0xc4 }; // RaspBerry
+  // Dirección Mac del dispositivo que se quiere encenceder
   byte mac[6] = { 0xd8, 0xbb, 0xc1, 0x96, 0x2c, 0xed }; // Mesa ether
-  // byte mac[] = { 0x8c, 0x1d, 0x96, 0xda, 0xe2, 0x24 }; // Mesa Wifi
 
 void sendWOL() {
     Serial.println("Sending WOL packet");
-
     byte WOLPacket[102];
-    // for (int i = 0; i < 6; i++)
-    //     WOLPacket[i] = 0xFF;
-    // for (int i = 1; i <= 16; i++)
-    //     for (int j = 0; j < 6; j++)
-    //         WOLPacket[i * 6 + j] = mac[j];
 
     memset(WOLPacket, 0xFF, 6);
     for (int i = 0; i < 16; i++) {
@@ -103,15 +93,11 @@ void loop() {
         }
         swtch = "0";
     }
-
-    //client.publish("SlowHedgehog/jose-csiot-pruebas", (estadoLed == HIGH? "Estado Led Jose: OFF" : "Estado Led Jose: ON"));
-
   }
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print(topic);
-  // Test: Mensaje
   Serial.print(": ");
   Serial.println(char(payload[0]));  // Primer caracter del mensaje [0]
 
@@ -141,23 +127,3 @@ void reconnect() {
     }
   }
 }
-
-
-// PRUEBA LED BUILD IN
-
-// void setup() {
-//   // put your setup code here, to run once:
-//   Serial.begin(115200);
-//   Serial.println("Hello, ESP32!");
-//   pinMode(LED_BUILTIN, OUTPUT);     // Numero de pin, variable en este caso, y el modo, output.
-// }
-
-// void loop() {
-//   // put your main code here, to run repeatedly:
-//   digitalWrite(LED_BUILTIN, HIGH);
-//   delay(1000); // en milisegundos, 1 seg
-//   digitalWrite(LED_BUILTIN, LOW);
-//   delay(1000);
-// }
-
-
